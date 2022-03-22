4ac4d93753c9342ea98847ffa01c10fd0a3465e7
Aakashkhatri77 <aakash.kc.77@gmail.com>
2022-03-22 17:42:52 +0545

    Add project files.

---
+++ UserAuth/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserAuth.Areas.Identity.Data;
+
+namespace UserAuth.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<AuthUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<AuthUser>
+{
+    public void Configure(EntityTypeBuilder<AuthUser> builder)
+    {
+        builder.Property(x => x.FirstName).HasMaxLength(255);
+        builder.Property(x => x.LastName).HasMaxLength(255);
+    }
+}
---
+++ UserAuth/Areas/Identity/Data/AuthUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace UserAuth.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the AuthUser class
+public class AuthUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
